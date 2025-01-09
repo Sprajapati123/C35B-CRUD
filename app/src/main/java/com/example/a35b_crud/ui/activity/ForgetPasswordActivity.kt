00e0e435336147b3feda41897c4d28df2ff8005a
sprajapati123 <sandisprajapati52@gmail.com>
2025-01-09 07:27:08 +0545

    navigation and login

--- app/src/main/java/com/example/a35b_crud/ui/activity/ForgetPasswordActivity.kt
+++ app/src/main/java/com/example/a35b_crud/ui/activity/ForgetPasswordActivity.kt
@@ -1,6 +1,5 @@
 package com.example.a35b_crud.ui.activity
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -8,60 +7,43 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.a35b_crud.R
-import com.example.a35b_crud.databinding.ActivityLoginBinding
+import com.example.a35b_crud.databinding.ActivityForgetPasswordBinding
 import com.example.a35b_crud.repository.UserRepositoryImpl
 import com.example.a35b_crud.utils.LoadingUtils
 import com.example.a35b_crud.viewmodel.UserViewModel
 
-class LoginActivity : AppCompatActivity() {
-    lateinit var binding: ActivityLoginBinding
+class ForgetPasswordActivity : AppCompatActivity() {
+    lateinit var forgetPasswordBinding: ActivityForgetPasswordBinding
     lateinit var userViewModel: UserViewModel
     lateinit var loadingUtils: LoadingUtils
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityLoginBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        forgetPasswordBinding = ActivityForgetPasswordBinding.inflate(layoutInflater)
+        setContentView(forgetPasswordBinding.root)
 
+        //initializing auth viewmodel
         var repo = UserRepositoryImpl()
         userViewModel = UserViewModel(repo)
 
         //initializing loading
         loadingUtils = LoadingUtils(this)
-
-        binding.btnLogin.setOnClickListener {
+        forgetPasswordBinding.btnForget.setOnClickListener {
             loadingUtils.show()
-            var email :String = binding.editEmail.text.toString()
-            var password :String = binding.editPassword.text.toString()
+            var email: String = forgetPasswordBinding.editEmailForget.text.toString()
 
-            userViewModel.login(email,password){
-                    success,message->
-                if(success){
-                    Toast.makeText(this@LoginActivity,message, Toast.LENGTH_LONG).show()
+            userViewModel.forgetPassword(email) { success, message ->
+                if (success) {
                     loadingUtils.dismiss()
-                    var intent = Intent(this@LoginActivity,NavigationActivity::class.java)
-                    startActivity(intent)
+                    Toast.makeText(this@ForgetPasswordActivity, message, Toast.LENGTH_LONG).show()
                     finish()
-                }else{
-                    Toast.makeText(applicationContext,message, Toast.LENGTH_LONG).show()
+                } else {
                     loadingUtils.dismiss()
+                    Toast.makeText(this@ForgetPasswordActivity, message, Toast.LENGTH_LONG).show()
 
                 }
             }
         }
-
-        binding.btnSignupnavigate.setOnClickListener {
-            val intent = Intent(this@LoginActivity,
-                RegisterActivity::class.java)
-            startActivity(intent)
-        }
-
-        binding.btnForget.setOnClickListener {
-            val intent = Intent(this@LoginActivity,
-                ForgetPasswordActivity::class.java)
-            startActivity(intent)
-        }
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
