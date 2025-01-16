cda66f1418b14fb3c643e947e7281368974e5c0e
sprajapati123 <sandisprajapati52@gmail.com>
2025-01-16 09:22:53 +0545

    navigation intnet

--- app/src/main/java/com/example/a35b_crud/ui/activity/ProductDashboardActivity.kt
+++ app/src/main/java/com/example/a35b_crud/ui/activity/ProductDashboardActivity.kt
@@ -1,17 +1,28 @@
 package com.example.a35b_crud.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.a35b_crud.R
+import com.example.a35b_crud.databinding.ActivityProductDashboardBinding
 
 class ProductDashboardActivity : AppCompatActivity() {
+    lateinit var binding: ActivityProductDashboardBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_product_dashboard)
+        binding = ActivityProductDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.floatingActionButton2.setOnClickListener {
+            var intent = Intent(this@ProductDashboardActivity,
+                AddProductActivity::class.java
+                )
+            startActivity(intent)
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
